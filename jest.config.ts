406d1f394a2a76f99e7040d626e7c61e1a69a0b7
ishiko <ishiko732@gmail.com>
2024-04-16 20:11:30 +0800

    config jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/*.js?(x)', '**/__tests__/*.ts?(x)'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'cobertura'],
   coverageThreshold: {
     global: {
       lines: 80,

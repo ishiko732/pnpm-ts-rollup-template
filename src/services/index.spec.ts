d03fd0e86ca6f8c3117f0fcd8c7e4d85b6a6d575
ishiko <ishiko732@gmail.com>
2024-08-12 23:23:27 +0800

    format

--- src/services/index.spec.ts
+++ src/services/index.spec.ts
@@ -1,10 +1,10 @@
-import { TestService } from '@services/index';
+import { TestService } from '@services/index'
 
 describe('services.ts', () => {
   test('ts-paths types', async () => {
-    const svc = new TestService();
-    const result = await svc.test();
-    expect(result.text).toBe('Hello World');
-    expect(result.id).toBe(1);
-  });
-});
+    const svc = new TestService()
+    const result = await svc.test()
+    expect(result.text).toBe('Hello World')
+    expect(result.id).toBe(1)
+  })
+})
